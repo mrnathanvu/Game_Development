bffe42b4d3ca607e17df1caa2e29e5f0f3a7c8bb
Nathan Vu <mr.nathanvu@gmail.com>
2021-03-24 22:40:18 -0700

    change title, logo, background

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import pygame
 
+# Initialize pygame package
 pygame.init()
 
 # Create a screen of 800(H) x 600(W)
 screen = pygame.display.set_mode((800, 600))
 
+# Title & Icon
+pygame.display.set_caption("Space Invaders")
+
+icon = pygame.image.load("img/spacecraft.png")
+pygame.display.set_icon(icon)
+
+# Game loop
 running = True
 while running:
     for event in pygame.event.get():
         # close button = quit event
         if event.type == pygame.QUIT:
             running = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.update()
