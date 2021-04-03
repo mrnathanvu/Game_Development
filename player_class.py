7222b6ff0c0a47dbe0c8dc17920b88bff2e954dd
Nathan Vu <mr.nathanvu@gmail.com>
2021-04-02 17:01:24 -0700

    update player movement

--- player_class.py
+++ player_class.py
@@ -11,9 +11,20 @@ class Player:
         self.grid_pos = pos
         self.pix_pos = self.get_pix_post()
         self.direction = vec(1, 0)
+        self.stored_direction = None
 
     def update(self):
         self.pix_pos += self.direction
+        if int(self.pix_pos.x + TOP_BOTTOM_BUFFER // 2) % self.app.cell_width == 0:
+            if self.direction == vec(1, 0) or self.direction == vec(-1, 0):
+                if self.stored_direction != None:
+                    self.direction = self.stored_direction
+
+        if int(self.pix_pos.y + TOP_BOTTOM_BUFFER // 2) % self.app.cell_height == 0:
+            if self.direction == vec(0, 1) or self.direction == vec(0, -1):
+                if self.stored_direction != None:
+                    self.direction = self.stored_direction
+
         # Setting grid position in reference to pix pos
         self.grid_pos[0] = (self.pix_pos[0] - TOP_BOTTOM_BUFFER + self.app.cell_width // 2) // self.app.cell_width + 1
         self.grid_pos[1] = (self.pix_pos[1] - TOP_BOTTOM_BUFFER + self.app.cell_height // 2) // self.app.cell_height + 1
@@ -31,7 +42,6 @@ class Player:
     def get_pix_post(self):
         return vec((self.grid_pos.x * self.app.cell_width) + TOP_BOTTOM_BUFFER // 2 + self.app.cell_width // 2,
                    (self.grid_pos.y * self.app.cell_height) + TOP_BOTTOM_BUFFER // 2 + self.app.cell_height // 2)
-        print(self.grid_pos, self.pix_pos)
 
     def move(self, direction):
-        self.direction = direction
+        self.stored_direction = direction
